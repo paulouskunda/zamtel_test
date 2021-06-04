ff319fe235c83d088e945ea1ce514d14b5beab9a
Paul Kunda <paulouskunda@gmail.com>
2021-06-04 15:37:19 +0200

    initial

---
+++ src/main/java/com/paul/zamtel_api/controller/ResourceController.java
@@ -0,0 +1,60 @@
+package com.paul.zamtel_api.controller;
+
+import com.paul.zamtel_api.model.DAOUser;
+import com.paul.zamtel_api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class ResourceController {
+//	@Autowired
+//	private RestTemplate restTemplate;
+
+	@Autowired
+	private UserRepository userDao;
+
+	@Autowired
+	private PasswordEncoder bcryptEncoder;
+
+	@RequestMapping("/hellouser")
+	public String getUser()
+	{
+		return "Hello User";
+	}
+	
+	@RequestMapping("/helloadmin")
+	public String getAdmin()
+	{
+		return "Hello Admin";
+	}
+
+	@PostMapping(path="/addUser")
+	public @ResponseBody  String addUser(@RequestParam String username, @RequestParam String password, @RequestParam String role ){
+		DAOUser user = new DAOUser();
+		user.setPassword(bcryptEncoder.encode(password));
+		user.setUsername(username);
+		if (role.equalsIgnoreCase("ROLE_ADMIN") || role.equalsIgnoreCase("ROLE_USER")){
+			userDao.save(user);
+			return "Saved";
+		}else{
+			return "Role is supposed to be either ROLE_ADMIN or ROLE_USER";
+		}
+	}
+
+	@GetMapping("/getOne")
+	public @ResponseBody String getOne(@RequestParam String username){
+		DAOUser user = new DAOUser();
+		return "Welcome "+username;
+	}
+
+//	@GetMapping("/fromExternal")
+//	public @ResponseBody String getExternal(){
+//		ZamtelEndPoint zamtelEndPoint = restTemplate.getForObject(
+//				"https://quoters.apps.pcfone.io/api/random", ZamtelEndPoint.class
+//		);
+//
+//		return zamtelEndPoint.toString();
+//	}
+
+}
---
+++ src/main/java/com/paul/zamtel_api/model/ZamtelEndPoint.java
@@ -0,0 +1,36 @@
+package com.paul.zamtel_api.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ZamtelEndPoint {
+    private String type;
+    private Value value;
+
+    public ZamtelEndPoint() {
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Value getValue() {
+        return value;
+    }
+
+    public void setValue(Value value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Quote{" +
+                "type='" + type + '\'' +
+                ", value=" + value +
+                '}';
+    }
+}
